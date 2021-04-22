31fabfbc65555bc61932b9acaf1e9d31b3913f51
Gabriel Monteiro <gabrielqmonteiro@gmail.com>
2021-04-22 02:26:12 -0300

    new update function/route and some minor changes

--- src/controllers/appointment.controller.js
+++ src/controllers/appointment.controller.js
@@ -10,34 +10,53 @@ const {DAILY_LIMIT: dailyLimit, HOUR_LIMIT: hourLimit } = process.env;
 
 class Appointment {
    
-    async index(req, res) {
+      async index(req, res) {
         const appointments = await AppointmentModel.find().sort({ appointmentTime: 'asc' });
         res.send({ data: appointments });
       }
 
       async store(req, res) {
         const body = req.body;
-        const scheduledAppointments = await AppointmentModel.find({appointmentDate :  body.appointmentDate}).exec();    
-        const appointmentByHour = scheduledAppointments.filter( ({appointmentTime}) => 
-        moment(appointmentTime).format('HH:mm') === moment(body.appointmentTime).format('HH:mm'));
+        try{
+          const scheduledAppointments = await AppointmentModel.find({appointmentDate :  body.appointmentDate}).exec();    
+          const appointmentByHour = scheduledAppointments.filter( ({appointmentTime}) => 
+          moment(appointmentTime).format('HH:mm') === moment(body.appointmentTime).format('HH:mm'));
         
-        if(scheduledAppointments.length > parseInt(dailyLimit)){
-         res.status(400).send({ message: 'O limite diário de atendimentos foi alcançado! Por favor, tente marcar em outro dia' });
-        }else{
-          if(appointmentByHour.length === parseInt(hourLimit)){
-            if(body.age < 60 || (appointmentByHour.filter( (ap) => ap.age < 60).length === 0)) {  //appointmentByHour[0].age >= 60 && appointmentByHour[1].age >= 60
-              res.status(400).send({ message: 'Este horário de atendimentos já está totalmente preenchido! Por favor, tente marcar em outro horário' });
-            }else {
+          if(scheduledAppointments.length > parseInt(dailyLimit)){
+            res.status(400).send({ message: 'O limite diário de atendimentos foi alcançado! Por favor, tente marcar em outro dia' });
+          }else{
+            if(appointmentByHour.length === parseInt(hourLimit)){
+              if(body.age < 60 || (appointmentByHour.filter( (ap) => ap.age < 60).length === 0)) {
+                res.status(400).send({ message: 'Este horário de atendimentos já está totalmente preenchido! Por favor, tente marcar em outro horário' });
+              }else{
               const youngerPerson = appointmentByHour.reduce((prev, curr) => prev.age < curr.age ? prev : curr);
               await Promise.all([await AppointmentModel.findOneAndDelete({_id:youngerPerson._id}), await AppointmentModel.create(body)]);
               res.send({message: 'Seu agendamento foi encaixado neste horário com sucesso!'});
+              }
+            }else{
+              const appointment = await AppointmentModel.create(body);
+              res.send({data: appointment, message: 'Agendamento realizado com sucesso!'});
             }
-          }else{
-            const appointment = await AppointmentModel.create(body);
-            res.send({data: appointment, message: 'Agendamento realizado com sucesso!'});
           }
+        }catch(error){
+          res.status(400).send({ message: error.message });
+        }
+        
+      }
+
+      async update(req, res) {
+        const {
+          body,
+          params: { _id: id },
+        } = req;
+    
+        try{
+          const appointment = await AppointmentModel.findByIdAndUpdate(id, body, { new: true });
+          res.send({ data: appointment, message: 'Status do atendimento alterado!' });
+        }catch(error){
+          res.status(400).send({ message: error.message });
         }
       }
-}
+    }
 
 module.exports = new Appointment();
--- src/routes/appointment.route.js
+++ src/routes/appointment.route.js
@@ -7,5 +7,6 @@ const Routes = express.Router();
 //Routes.post("/appointment", AppointmentController.store);
 Routes.get("/appointment", AppointmentController.index);
 Routes.post("/appointment", AppointmentController.store);
+Routes.put("/appointment/:_id", AppointmentController.update);
 
 module.exports = Routes;
